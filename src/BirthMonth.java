91dae9a8c77f3e956cc1491b2829c57dd85e8b2d
mzerhusen <zerhusma@mail.uc.edu>
2025-02-05 20:02:34 -0500

    Completed

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -1,6 +1,7 @@
 import java.util.Scanner;
 
-public class BirthMonth {
+public class BirthMonth
+{
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
         int birthMonth;
