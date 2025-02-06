e764e547c6e6b234af1ec945189de2eef8db64f5
mzerhusen <zerhusma@mail.uc.edu>
2025-02-05 20:03:48 -0500

    Completed

--- src/BirthMonth.java
+++ src/BirthMonth.java
@@ -9,14 +9,20 @@ public class BirthMonth
         String invalidInput;
 
         System.out.println("Please enter your birth month as a number from 1 to 12: ");
-        if (in.hasNextInt()) {
+        if (in.hasNextInt())
+        {
             birthMonth = in.nextInt();
-            if (birthMonth >= 1 && birthMonth <= 12) {
+            if (birthMonth >= 1 && birthMonth <= 12)
+            {
                 System.out.println("You entered your birth month as " + birthMonth);
-            } else {
+            }
+            else
+            {
                 System.out.println("You entered an invalid birth month. You entered " + birthMonth);
             }
-        } else {
+        }
+        else
+        {
             invalidInput = in.nextLine();
             System.out.println("You have made an invalid entry, you entered " + invalidInput);
         }
